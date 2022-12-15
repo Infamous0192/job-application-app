14c9e25d9f9e57629fb191d81e307f3e5f32df8c
Next-Fighter <akhmad.khairul.mujidi@gmail.com>
2022-12-15 22:31:00 +0800

    delete

--- Models/Job.cs
+++ Models/Job.cs
@@ -1,14 +1,12 @@
 namespace Models
 {
-  public class Job
-  {
-    public int Id { get; set; }
-    public string Title { get; set; } = default!;
-    public string Description { get; set; } = default!;
-    public string Field { get; set; } = default!;
-    public int Salary { get; set; }
-    public string Location { get; set; } = default!;
-
-    public int CompanyId { get; set; }
-  }
+    public class Job
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = default!;
+        public string Description { get; set; } = default!;
+        public string Field { get; set; } = default!;
+        public int Salary { get; set; }
+        public int CompanyId { get; set; }
+    }
 }
